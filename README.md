c5c1371930381010544be4355a7f32d8aac9ee1b
Dinael URDANETA <dinael@gmail.com>
2023-08-22 08:43:49 +0200

    feat: app config

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Podcaster player
+# Poscater App
+
+With this application, you can view and listen to the top 100 most popular podcasts on iTunes.
+
+## Summary
+
+With this application, you can view and listen to the top 100 most popular podcasts on iTunes.
+
+This app is a side-project, it will be improved little by little, at the moment you can check the top 100 iTunes podcasts and play their episodes.
+
+Everything is in SPA made with React, here's what you need to run it.
+
+## Scripts
+### `Install dependencies`
+
+Open the application in your terminal and run this command **'npm install'** or **'npm i'** to install all dependencies.
+
+### `Run project in dev mode`
+
+Run this command **'npm start'** to development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+
+### `Test`
+
+Use **'npm test'** to run test you need launch the test runner in the interactive watch mode.\
+
+For this project, jest was used to test [Jest](https://jestjs.io/).
+
+For more information, you can consult the documentation [here](https://jestjs.io/es-ES/docs/getting-started)
+
+### `npm run build`
+
+Run **'npm run build'** to build this app for production.
+
+### Libraries
+* [React](https://react.dev/)
+* [React router DOM](https://reactrouter.com/)
+* [Jest](https://jestjs.io/)
+
---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss/plugin")
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Quicksand', 'sans-serif']
+    },
+    colors: {
+      'transparent': 'transparent',
+      'current': 'currentColor',
+      'positive': 'rgba(255, 255, 255, 1)',
+      'negative': 'rgba(0, 0, 0, 1)',
+      'alt': 'rgba(255, 255, 255, 0.3)',
+      'accent': 'rgba(92, 103, 222, 1)',
+      'accent-dark': 'rgba(60, 80, 194, 1)',
+      'verification': '#138BD7',
+      'test': 'deeppink',
+      'dark': {
+        DEFAULT: 'rgba(26, 26, 26, 1)',
+        'alt': 'rgba(26, 26, 26, 0.3)',
+      },
+      'app': {
+        'text': 'rgba(255, 255, 255, 1)',
+        'bg': 'rgba(1, 1, 1, 1)'
+      }
+    }
+  },
+  plugins: [
+    plugin(({ addBase, theme }) => {
+      addBase({
+        body: {
+          color: theme("colors.app.text"),
+          backgroundColor: theme("colors.app.bg"),
+        },
+        '#root': {
+          display: 'grid',
+          gridAutoFlow: 'row dense',
+          gridTemplateRows: '1fr 6.875rem',
+          height: '100vh',
+        },
+        '*:focus': {
+          outline: '0'
+        }
+      });
+    }),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
+
